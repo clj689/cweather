c46ce7f25d7cbf128c728d4d07830c564764ac72
Lucas Chen <clj689@gmail.com>
2018-05-28 11:55:17 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/lucas/com/cweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package lucas.com.cweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2018/5/26 026.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
